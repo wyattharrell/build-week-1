814de6b1e115fa87df0d27371916ba0060626dc0
Christopher DeVito <toolofjesus@protonmail.com>
2020-03-02 14:25:32 -0500

    commented out error in master until I can fix it.

--- Simple Home Calculator/Simple Home Calculator/View Controllers/MortgageCalculatorViewController.swift
+++ Simple Home Calculator/Simple Home Calculator/View Controllers/MortgageCalculatorViewController.swift
@@ -102,7 +102,7 @@ class MortgageCalculatorViewController: UIViewController {
             propertyTax = Double(propertyTaxString) ?? 0.0
         }
         
-        mortgageLoanController.updateMortgageLoan(mortgageLoan: mortgageLoanController.mortgageLoan!, amount: loanAmount, downPayment: downPayment, interestRate: interestRate, mortgageLength: mortgageLength, monthlyHOA: monthlyHOA, homeInsurance: homeInsurance, propertyTax: propertyTax)
+        //mortgageLoanController.updateMortgageLoan(mortgageLoan: mortgageLoanController.mortgageLoan!, amount: loanAmount, downPayment: downPayment, interestRate: interestRate, mortgageLength: mortgageLength, monthlyHOA: monthlyHOA, homeInsurance: homeInsurance, propertyTax: propertyTax)
         
     }
     
