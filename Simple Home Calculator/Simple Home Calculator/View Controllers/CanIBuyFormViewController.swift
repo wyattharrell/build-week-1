5b1781c1e7f3104964c48951d4283bdb6bc23c93
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-02 22:55:21 -0500

    almost completed formulas and calculations

--- Simple Home Calculator/Simple Home Calculator/View Controllers/CanIBuyFormViewController.swift
+++ Simple Home Calculator/Simple Home Calculator/View Controllers/CanIBuyFormViewController.swift
@@ -28,20 +28,20 @@ class CanIBuyFormViewController: UIViewController {
     
     var income: Int?
     var maxMonthlyPaymentBasedOnIncome_M1: Double?
+    
     var maxMonthlyPaymentBasedOnDebt_M2: Double = 0
     var maxPIPaymentBasedOnExpenses_M3: Double = 0
     var maxPIPaymentBasedOnFunds_M4: Double = 0
-
     var lowerOfM1M2: Double = 0
     var lowerOfM3M4: Double = 0
-    
+    var loanAmountBasedOnMaxPIPayment: Double = 0
+    var downPaymentBasedOnAvailFunds: Double = 0
+    var totalEstimatedClosingCosts: Double = 0
+    var maxHomePrice: Double = 0
     var maxHomePriceBasedOnFunds: Double = 0
-    
-    
-    
-    
-    
-    var maxMonthlyPayment: Double = 0
+
+    var maxDebtToIncomeRatio: Double = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,10 +51,28 @@ class CanIBuyFormViewController: UIViewController {
     
     @IBAction func calculateButtonTapped(_ sender: Any) {
         guard let income = income else { return }
+        guard let maxMonthlyPaymentBasedOnIncome_M1 = maxMonthlyPaymentBasedOnIncome_M1 else { return }
+        guard let monthlyDebts = monthlyDebtsTextField.text, let monthlyDebts_Int = Int(monthlyDebts) else { return }
+        
+        switch maxDebtToIncomeRatioSegmentedControl.selectedSegmentIndex {
+            case 0:
+                maxDebtToIncomeRatio = 0.36
+            case 1:
+                #warning("MUST CALL A UIALERT")
+            default:
+                print("N/A")
+        }
+        
+        maxMonthlyPaymentBasedOnDebt_M2 = calculateMaxMonthlyPaymentBasedOnDebt_M2(income: income, debt: monthlyDebts_Int, maxDTIRatio: maxDebtToIncomeRatio)
+        lowerOfM1M2 = calculateLowerOfM1M2(m1: maxMonthlyPaymentBasedOnIncome_M1, m2: maxMonthlyPaymentBasedOnDebt_M2)
+        
+        guard let propertyTax = propertyTaxesTextField.text, let propertyTax_Double = Double(propertyTax), let hoa = homeOwnersInsuranceTextField.text, let hoa_Double = Double(hoa), let privateMortgageInsurance = privateMortgageInsuranceTextField.text, let privateMortgageInsurance_Double = Double(privateMortgageInsurance), let hoaFees = hoaFeesTextField.text, let hoaFees_Double = Double(hoaFees), let otherFees = otherFeesTextField.text, let otherFees_Double = Double(otherFees) else { return }
+        
+        let expenses = propertyTax_Double + hoa_Double + hoaFees_Double + privateMortgageInsurance_Double + otherFees_Double
+
+        maxPIPaymentBasedOnExpenses_M3 = calculatePIPaymentBasedOnExpenses_M3(lowerOfM1orM2: lowerOfM1M2, expenses: expenses)
         
         
-        
-        // maxMonthlyPaymentBasedOnDebt_M2 = calculateMaxMonthlyPaymentBasedOnDebt_M2(income: income, debt: 400, maxDTIRatio: 0.36)
     }
     
     // MARK: - Navigation
@@ -90,10 +108,26 @@ class CanIBuyFormViewController: UIViewController {
         return min(m3,m4)
     }
     
-    func calculatemMaxPIPaymentBasedOnFunds_M4(r: Double, n: Double, pv: Double) -> Double {
+    func calculateMaxPIPaymentBasedOnFunds_M4(r: Double, n: Double, pv: Double) -> Double {
         let temp = (1 + r)
         let negativeN = 0 - n
         return pv * (r / (1 - pow(temp, negativeN)))
+    }
+    
+    func calculateLoanAmountBasedOnMaxPIPayment(maxHomePriceBasedOnFundss: Double, minDownPayment: Double) -> Double {
+        return maxHomePriceBasedOnFundss * (1-minDownPayment) //pass 0.2 not 20
+    }
+    
+//    func calculateDownPaymentBasedOnAvailFunds() -> Double {
+//
+//    }
+//
+//    func calculateTotalEstimatedClosingCosts() -> Double {
+//
+//    }
+    
+    func calculateMaxHomePrice(a: Double, b: Double) -> Double {
+        return a + b //totalEstimatedClosingCosts + downPaymentBasedOnAvailFunds
     }
     
     
