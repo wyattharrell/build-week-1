2bc761ff39b751f0d4ae17288fecbabf5fde31ca
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-04 14:03:09 -0500

    great stopping point for cost of living calc

--- Simple Home Calculator/Simple Home Calculator/View Controllers/CostOfLivingResultsViewController.swift
+++ Simple Home Calculator/Simple Home Calculator/View Controllers/CostOfLivingResultsViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class CostOfLivingResultsViewController: UIViewController {
 
+    
+    @IBOutlet weak var arrowImage: UIImageView!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var increaseOrDecreaseLabel: UILabel!
+    
     var currentState: State?
     var futureState: State?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(currentState?.State)
-        print(futureState?.State)
-        
+        updateViews()
     }
 
+    func updateViews() {
+        guard let futureState = futureState, let currentState = currentState else { return }
+        
+        stateLabel.text = "\(currentState.State) vs. \(futureState.State)"
+        if currentState.costRank < futureState.costRank {
+            increaseOrDecreaseLabel.text = "Cost Of Living Increase"
+            arrowImage.image = UIImage(systemName: "arrow.up")
+        } else {
+            increaseOrDecreaseLabel.text = "Cost Of Living Decrease"
+            arrowImage.image = UIImage(systemName: "arrow.down")
+        }
+
+    }
 
 }
